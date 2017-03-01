3be9e35fc7def7ceed385dfd1a6844662a227e44
J.Coleman <jcoleman@dovico.com>
2017-03-01 19:21:49 -0400

    updates

--- js/controllers.js
+++ js/controllers.js
@@ -1,5 +1,18 @@
 angular.module('starter.controllers', [])
 
 .controller('AppCtrl', ['$scope', function($scope){
-	console.log("success")
+	// console.log("success")
+	$scope.menuItems = [{
+		text: "About The Depot",
+		url: "#"
+	},{
+		text: "Pointless Projects",
+		url: "#"
+	},{
+		text: "GitHub",
+		url: "https://github.com/Dovico/CodeDepot"
+	},{
+		text: "Slack",
+		url: "#"
+	}]
 }])
